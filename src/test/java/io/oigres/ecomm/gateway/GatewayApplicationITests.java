78e53bf4dc88ed988f87384d1ceca6c3db5477ef
Sergio <sjexpos@gmail.com>
2024-10-08 16:04:07 -0300

    Update documentation

--- src/test/java/io/oigres/ecomm/gateway/GatewayApplicationITests.java
+++ src/test/java/io/oigres/ecomm/gateway/GatewayApplicationITests.java
@@ -2,9 +2,13 @@ package io.oigres.ecomm.gateway;
 
 import org.junit.jupiter.api.Test;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.TestPropertySource;
 
 @SpringBootTest
-class GatewayApplicationTests {
+@TestPropertySource(
+		locations = "classpath:application-integrationtest.properties"
+)
+class GatewayApplicationITests {
 
 	@Test
 	void contextLoads() {
